365bfddb96e1c8380a520725181e143366c18b6f
1ulce <shun.1ulce@gmail.com>
2016-09-21 12:15:43 +0900

    [re-update] production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,12 +78,12 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   ####### 1ulce add ####################
-  config.action_mailer.default_url_options = { host: '自分のHerokuアプリのドメイン' }
+  config.action_mailer.default_url_options = { host: 'gentle-eyrie-12804.herokuapp.com' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
   {
    user_name: ENV['SENDGRID_USERNAME'],
-   password: ENV['SENDGRID_USERNAME'],
+   password: ENV['SENDGRID_PASSWORD'],
    domain: "heroku.com",
    address: "smtp.sendgrid.net",
    port: 587,
