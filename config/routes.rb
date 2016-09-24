74695b6ff1e4b0b54bc98f8578ca300c1e491427
1ulce <shun.1ulce@gmail.com>
2016-09-24 14:24:23 +0900

    [create] user relationships

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  
   devise_for :users, controllers: {
     registrations: "users/registrations",
     omniauth_callbacks: "users/omniauth_callbacks"
   }
+
   resources :blogs do
     resources :comments
     
@@ -12,14 +14,19 @@ Rails.application.routes.draw do
       post :confirm
     end
   end
+
   resources :contacts, only: [:new, :create] do
     collection do
       post :confirm
     end
   end
+
+  resources :users, only: [:index]
+
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
+  
   root 'top#index'
   
   # The priority is based upon order of creation: first created -> highest priority.
