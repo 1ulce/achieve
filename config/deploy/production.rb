5dab6cb1305a80afbcd6d1b986c3e15ac375ee2d
1ulce <shun.1ulce@gmail.com>
2016-09-27 12:08:42 +0900

    [update] setting for production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,2 @@
-server 'http://52.196.153.74', user: 'ec2-user', roles: %w{app db web}
-set :ssh_options, keys: '/Users/ch0chip/.ssh/1ulce_aws_key.pem'
+server '52.196.153.74', user: 'app', roles: %w{app db web}
+set :ssh_options, keys: '/Users/ch0chip/.ssh/app_aws_key'
